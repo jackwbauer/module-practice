d1b547ba8c8e12b6e9230c389471630b1813a139
Jack Bauer <bauer.jackw@gmail.com>
2018-10-26 20:10:06 +0000

    fixed sort

--- module-practice.js
+++ module-practice.js
@@ -6,13 +6,7 @@ function addToList(num) {
 
 function sortedNumList() {
   var sortedList = numList.sort(function(a, b) {
-    if(numList[a] > numList[b]) {
-      return 1;
-    }
-    if(numList[a] < numList[b]) {
-      return -1;
-    }
-    return 0;
+    return a - b;
   });
   return numList;
 }
